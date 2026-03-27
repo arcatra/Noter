a85c7341d79231e1120e7548ac1fc8f6f95d57d6
Deepak <deep.vemu.cse@gmail.com>
2026-03-27 22:27:01 +0530

    IMP: args support Now we can run the project with runNoter.sh bash file, along with the args(commands)

--- app/src/main/java/noter/Noter.java
+++ app/src/main/java/noter/Noter.java
@@ -3,6 +3,10 @@ package noter;
 // Imports ----------
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 // import java.io.BufferedWriter;
 
@@ -10,7 +14,7 @@ import java.io.FileReader;
 
 public class Noter {
     public void displayAbout() {
-        try (BufferedReader bfr = new BufferedReader(new FileReader("src/main/resources/about.txt"))) {
+        try (BufferedReader bfr = new BufferedReader(new FileReader("./src/main/resources/about.txt"))) {
             String line;
 
             while ((line = bfr.readLine()) != null) {
@@ -22,8 +26,57 @@ public class Noter {
         }
     }
 
+    public void addTask(String task) {
+        System.out.println("Task : " + task);
+
+    }
+
+    public void destructArgs(String args[]) {
+        int len = args.length;
+
+        for (int index = 0; index < len; index++) {
+            // System.out.println("Current: " + args[index]);
+            String val = args[index];
+
+            if (val.startsWith("-")) {
+                val = val.toLowerCase();
+            }
+
+            switch (val) {
+                case "-about":
+                    this.displayAbout();
+
+                    break;
+
+                case "-task":
+                    if (index + 1 < len) {
+                        String taskName = args[index + 1];
+                        System.out.println("Adding task: " + taskName);
+
+                    } else {
+                        System.out.println("Error: Couldn't find the task");
+
+                    }
+
+                    break;
+
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
+        Scanner userIn = new Scanner(System.in);
+        userIn.close();
+
         Noter obj = new Noter();
-        obj.displayAbout();
+
+        if (args.length >= 1) {
+            obj.destructArgs(args);
+
+        } else {
+            System.out.println("MSG: Could'nt find any args");
+        }
+
     }
 }
